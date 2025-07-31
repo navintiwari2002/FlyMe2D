e5f69c27cda1f15461db5aa10ab4d5e9b57f52bd
Navin Kumar <navinkumarhjp5@gmail.com>
2025-07-31 20:05:24 +0530

    Initial commit of Fly_Me Unity game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.linearVelocity = Vector2.up * jumpForce;
+        }
+    }
+}
